5f72bd9d96f42626e7ca188b28d7beb85e1c8c0b
Chris Clifford and Joshua Rinard <chris@>
2016-11-17 15:11:40 -0800

    add UI and integration test

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/coin_combinations')
+require('./lib/anagrams')
 also_reload('lib/**/*.rb')
 
 get('/') do
@@ -8,6 +8,9 @@ get('/') do
 end
 
 get ('/result') do
-
-   erb(:index)
+  @word = params.fetch('word')
+  @result1 = (@word).anagram(params.fetch('anagram1'))
+  @result2 = (@word).anagram(params.fetch('anagram2'))
+  @result3 = (@word).anagram(params.fetch('anagram3'))
+  erb(:index)
 end
